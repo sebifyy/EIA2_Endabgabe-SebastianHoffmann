d9758cbd8e985956522bfaf1b086a75788671754
sebify <sebastianhffm1@gmail.com>
2021-02-13 02:35:19 +0100

    Serverlink geändert, styling angepasst

--- Particle/Particle.ts
+++ Particle/Particle.ts
@@ -43,7 +43,7 @@ namespace Firework {
                     crc2.closePath();
                     crc2.fillStyle = this.color;
                     crc2.shadowColor = this.glowColor;
-                    crc2.shadowBlur = 15 * this.particleSize / 100 * Math.random() * 150;
+                    crc2.shadowBlur = 15 * this.particleSize / 100 + Math.random() * 200;
                     crc2.fill();
                     crc2.restore();
                     console.log(this.type);
@@ -59,25 +59,25 @@ namespace Firework {
                     crc2.closePath();
                     crc2.fillStyle = this.color;
                     crc2.shadowColor = this.glowColor;
-                    crc2.shadowBlur = 15 * this.particleSize / 100 * Math.random() * 150;
+                    crc2.shadowBlur = 15 * this.particleSize / 100 + Math.random() * 200;
                     crc2.fill();
                     crc2.restore();
                     console.log(this.type);
                     break;
                 case "square":
-                        crc2.save();
-                        crc2.beginPath();
-                        crc2.translate(this.position.x, this.position.y);
-                        crc2.scale(0.0015 * this.particleSize, 0.0015 * this.particleSize);
-                        crc2.rect(-100, -100, 100, 100);
-                        crc2.closePath();
-                        crc2.fillStyle = this.color;
-                        crc2.shadowColor = this.glowColor;
-                        crc2.shadowBlur = 15 * this.particleSize / 100 * Math.random() * 150;
-                        crc2.fill();
-                        crc2.restore();
-                        console.log(this.type);
-                        break;
+                    crc2.save();
+                    crc2.beginPath();
+                    crc2.translate(this.position.x, this.position.y);
+                    crc2.scale(0.0015 * this.particleSize, 0.0015 * this.particleSize);
+                    crc2.rect(-100, -100, 100, 100);
+                    crc2.closePath();
+                    crc2.fillStyle = this.color;
+                    crc2.shadowColor = this.glowColor;
+                    crc2.shadowBlur = 15 * this.particleSize / 100 + Math.random() * 200;
+                    crc2.fill();
+                    crc2.restore();
+                    console.log(this.type);
+                    break;
                 case "gme":
                     crc2.save();
                     audioGME.play();
@@ -86,7 +86,7 @@ namespace Firework {
                     crc2.ellipse(-23 * this.particleSize / 100, 23 * this.particleSize / 100, 7 * this.particleSize / 100, 35 * this.particleSize / 100, Math.PI / 4, 0, 2 * Math.PI);
                     crc2.fillStyle = this.color;
                     crc2.shadowColor = this.glowColor;
-                    crc2.shadowBlur = 500 * this.particleSize / 100 * Math.random() * 150;
+                    crc2.shadowBlur = 15 * this.particleSize / 100 + Math.random() * 200;
                     crc2.closePath();
                     crc2.fill();
                     crc2.drawImage(imageGME, -50 * this.particleSize / 100, -50 * this.particleSize / 100, this.particleSize, this.particleSize)
