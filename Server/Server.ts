e9f0feb7416c3d67e55ceb04aeea868618187a80
sebify <sebastianhffm1@gmail.com>
2021-02-15 12:04:08 +0100

    Server änderung auf heroku

--- Server/Server.ts
+++ Server/Server.ts
@@ -9,7 +9,7 @@ export namespace Firework {
     }
 
     let fireworkCollection: Mongo.Collection;
-    let databaseUrl: string = "mongodb+srv://DerPapa:asdfyxcv@eia2-2020-2021-endabgab.a47ra.mongodb.net/EIA2-2020-2021-Endabgabe?retryWrites=true&w=majority";
+    let databaseUrl: string = "mongodb+srv://sebify:asdf@cluster0.21tky.mongodb.net/Feuerwerk?retryWrites=true&w=majority";
     let port: number | string | undefined = process.env.PORT;
     if (port == undefined)
         port = 5001;
